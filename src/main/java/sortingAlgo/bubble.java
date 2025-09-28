ff045fe32459c8c6f3e225e1afccdfba512f0e98
leonard2312 <leonardoguatemala91@aragon.unam.mx>
2025-09-28 12:26:41 -0600

    iteration

--- src/main/java/sortingAlgo/bubble.java
+++ src/main/java/sortingAlgo/bubble.java
@@ -1,5 +1,18 @@
 package sortingAlgo;
 
-public class bubble {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class bubble {
+    public static void main(String []args){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Scanner lecture = new Scanner(System.in);
+        System.out.println("How many numbers you want");
+        int itN = lecture.nextInt();
+        for(int y=0;y<itN;y++){
+            System.out.println("Digit a number");
+            numbers.add(lecture.nextInt());
+        }
+        System.out.println("numbers = " + numbers);
+    }
 }
